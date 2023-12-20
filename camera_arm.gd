785d0d94aca4de130d155d7cf3d9b03b62bf70db
Topicranger <43324723+Topicranger@users.noreply.github.com>
2023-12-20 03:21:09 +0100

    just dumping some good progress 🦾

---
+++ camera_arm.gd
@@ -0,0 +1,25 @@
+extends SpringArm3D
+
+const SPEED = 3.5
+
+@export var pitch_length: Curve
+
+func _ready():
+	text()
+	pitch_length.bake()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var input_dir = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
+	if input_dir:
+		input_dir = input_dir.limit_length()
+		rotate(Vector3.DOWN, input_dir.x*delta*SPEED)
+		rotate_object_local(Vector3.LEFT, input_dir.y*delta*SPEED)
+		rotation.x = clampf(rotation.x, -1.5, 1)
+	
+	var spring_target_length = 7*pitch_length.sample((-rotation.x+PI/2)/PI)
+	spring_length = lerpf(spring_length, spring_target_length, 0.1)
+	text()
+
+func text():
+	$Label.text = "pitch: %f\npitch_deg: %f\npitch+pi: %f\nlength: %f" % [rotation.x, rotation_degrees.x, (-rotation.x+PI/2)/PI, spring_length]
---
+++ sylvia.gd
@@ -0,0 +1,58 @@
+extends CharacterBody3D
+
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var _state_chart: StateChart = $StateChart
+@onready var _animation_tree: AnimationTree = $AnimationTree
+@onready var _animation_state_machine: AnimationNodeStateMachinePlayback = _animation_tree.get("parameters/playback")
+@onready var _mesh: MeshInstance3D = $MeshInstance3D
+@onready var _camera: Camera3D = get_viewport().get_camera_3d()
+
+func _physics_process(delta):
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("player_left", "player_right", "player_up", "player_down")
+	var camera_pos = _camera.get_camera_transform().basis
+	var direction = ((camera_pos * Vector3(input_dir.x, 0, input_dir.y))*1.2).limit_length()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		_mesh.look_at(to_global(direction))
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		_state_chart.send_event("airborne")
+	else:
+		_state_chart.send_event("grounded")
+	
+	# let the state machine know if we are moving or not
+	if velocity.length_squared() <= 0.005:
+		_state_chart.send_event("idle")
+	else:
+		_state_chart.send_event("moving")
+
+## Called in states that allow jumping, we process jumps only in these.
+func _on_jump_enabled_state_physics_processing(_delta):
+	if Input.is_action_just_pressed("player_jump"):
+		$Jump.play()
+		velocity.y = JUMP_VELOCITY
+		_state_chart.send_event("jump")
+
+## Called when the jump transition is taken in the double-jump
+## state. Only used to play the double jump animation.
+func _on_double_jump_jump():
+	$DoubleJump.play()
+##	_animation_state_machine.travel("DoubleJump")
+	#pass
